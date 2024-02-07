daac4c99ba05f8a8a050b9928ce17d9fe8cae848
AyushChauhan3757 <ayushc262@gmail.com>
2024-02-08 00:09:36 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,34 @@
+// next.config.js
+
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io", // Add utfs.io here
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
